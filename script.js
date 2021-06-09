b721db1d314df479b1dbeaee766dcd928eddd2d1
navedrahim <nrah1988@gmail.com>
2021-06-08 21:38:02 -0400

    event listener

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const list = document.querySelector('#show-brew');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
-
+// make an axios call and console log result
+// make a callback function that can take the data and attach it to the DOM
 // create event listener for button
 function showBrew(e) {
   e.preventDefault();
